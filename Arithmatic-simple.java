3baeee321c886a84a7005a081188dd38501f9164
Anukrit Sharma <153848978+an21052002@users.noreply.github.com>
2023-12-14 22:58:18 -0800

    Add files via upload

---
+++ Arithmatic-simple.java
@@ -0,0 +1,10 @@
+class Mathematical{
+    public static void main(String args[]){
+        int x=2;
+        int y=8;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x%y);
+    }
+}
